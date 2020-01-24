6c6da8184dbbfef1a348876b99900b2b7d76814f
rixiobarrios <rixiobarrios.consulado@gmail.com>
2020-01-24 13:28:35 -0600

    Adding Components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Form from './components/Form';
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Hello World</h1>
-            </header>
-        </div>
-    );
+class App extends Component {
+    getRecipe = e => {
+        e.preventDefault();
+        const recipeName = e.target.elements.recipeName.value;
+        console.log('I am App');
+    };
+    render() {
+        return (
+            <div className="app">
+                <header className="header">
+                    <h1 className="title">Recipe Search</h1>
+                </header>
+                <Form getRecipe={this.getRecipe} />
+            </div>
+        );
+    }
 }
 
 export default App;
