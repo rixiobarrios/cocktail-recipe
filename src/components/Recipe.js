6df009aa4bd5dc5b0db8aa09baa9fef882b62902
rixiobarrios <rixiobarrios.consulado@gmail.com>
2020-01-26 16:14:20 -0600

    Last Page console logging correct response

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const API_KEY = '1';
+// const API_KEY = '1';
 
 class Recipe extends React.Component {
     state = {
@@ -8,18 +8,17 @@ class Recipe extends React.Component {
     };
 
     componentDidMount = async () => {
-        const title = this.props.location.state.recipe;
+        const strDrink = this.props.location.state.recipe;
         const req = await fetch(
-            `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${title}`
+            `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${strDrink}`
         );
         const res = await req.json();
-        this.setState({ activeRecipe: res.recipes[0] });
-        console.log(this.state.activeRecipe);
+        this.setState({ activeRecipe: res.recipe });
+        console.log('I am a recipe for the ', this.props.location.state.recipe);
     };
 
     render() {
         return <div>Recipe Component!</div>;
     }
 }
-
 export default Recipe;
